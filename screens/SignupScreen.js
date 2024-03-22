fb4194d853d5085cb435b10cac885c9f99447024
Anurag Shrestha <ashrestha@unm.edu>
2024-03-21 23:18:31 -0600

    Added async function to create a new user

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,24 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/Ui/LoadingOverlay";
+
 
 function SignupScreen() {
-  return <AuthContent />;
+
+  const[isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signUpHandler({email, password}) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+   if(isAuthenticating){
+    return <LoadingOverlay message= "Creating user name"/>
+   }
+
+  return <AuthContent onAuthenticate={signUpHandler} />;
 }
 
 export default SignupScreen;
