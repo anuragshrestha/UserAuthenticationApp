b5835d736baa2832178175e0d04c35da05feb346
Anurag Shrestha <ashrestha@unm.edu>
2024-03-25 19:13:46 -0600

    Added authentication to check if the user gives incorrect credentials while logIn

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,30 @@
 import AuthContent from '../components/Auth/AuthContent';
+import { useState } from 'react';
+import LoadingOverlay from '../components/Ui/LoadingOverlay';
+import { login } from '../util/auth';
+import { Alert } from 'react-native';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+
+  const[isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({email, password}) {
+
+    setIsAuthenticating(true);
+    try{
+      await login(email, password);
+    } catch(err){
+      Alert.alert("Authentication Failed!", "Either your email or password is incorrect.")
+    }
+    setIsAuthenticating(false);
+  }
+
+   if(isAuthenticating){
+    return <LoadingOverlay message= "Loggin in..."/>
+   }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
+
 
 export default LoginScreen;
