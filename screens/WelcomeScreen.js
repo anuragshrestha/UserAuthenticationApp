be5ff0738ca078405f702584cd686c63fa7d7684
Anurag Shrestha <ashrestha@unm.edu>
2024-04-25 17:44:14 -0600

    added a message to the welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 function WelcomeScreen() {
+
+  const[fetchedMessage, setFetchedMessage] = useState('');
+
+  useEffect(() => {
+      axios.get('https://reactnative-expense-app-default-rtdb.firebaseio.com/message.json').then((response) => {
+        console.log(response.data);
+      });
+  }, [ ]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
